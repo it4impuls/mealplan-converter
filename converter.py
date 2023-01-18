47771717fa1337a25caa7c13c94ef7c0ef643760
fknupper <110169930+fknupper@users.noreply.github.com>
2023-01-18 15:13:12 +0100

    start with camelot

--- converter.py
+++ converter.py
@@ -1,7 +1,20 @@
+import camelot
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+
+
+
 class Converter:
 
-    def convert(input, output):
+    def convert(input):
         try:
+            tables = camelot.read_pdf(input)
+            print(tables[0])
             raise Exception("Sorry")
         except(FileNotFoundError):
             raise Exception("File Not found!")
+
+if __name__ == "__main__":
+    tk.Tk().withdraw()
+    print(askopenfilename())
+    # Converter.convert("'" + askopenfilename() + "'")
