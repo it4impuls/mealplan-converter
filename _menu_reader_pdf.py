5076a70578068b85f85ce062eb2427fbb0129dd6
fknupper <fknupper@posteo.de>
2023-01-19 18:34:40 +0100

    Remove logcolors

--- _menu_reader_pdf.py
+++ _menu_reader_pdf.py
@@ -1,21 +1,21 @@
 """ Was macht die Datei. """
 import os
 import logging
-import coloredlogs
 import camelot
-# import xlsxwriter
 
+
+# import xlsxwriter
 
 class MenuReaderPDF:
     """ Klasse zum einlesen einer PDF-Datei. """
 
     def __init__(self, pfadzurdatei):
         self.pfadzurdatei = pfadzurdatei
-    
+
     def openreader(self):
         """ Funktion zum entgegennehmen eines Dateipfades. """
         try:
-            if os.path.exists(self.pfadzurdatei): # eventuell in init
+            if os.path.exists(self.pfadzurdatei):  # eventuell in init
                 tables = camelot.read_pdf(self.pfadzurdatei)
                 logging.debug(tables[0].df)
             else:
@@ -24,13 +24,10 @@ class MenuReaderPDF:
         except Exception:
             logging.error('Fehler unbekannt!')
             raise Exception
-        
+
 
 if __name__ == "__main__":
-
     logger = logging.getLogger()
-    coloredlogs.install(level='DEBUG')
-    coloredlogs.install(level='DEBUG', logger=logger)
     logging.basicConfig(
         format='%(asctime)s %(levelname)s: %(message)s',
         level=logging.DEBUG
