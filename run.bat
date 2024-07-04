6ca479f9d24ebdb59199d6af5c38a5edb220dbe9
marilynias <marilynia57+github@gmail.com>
2024-07-04 15:26:53 +0200

    final

--- run.bat
+++ run.bat
@@ -1,15 +1,38 @@
 @echo off
 SET SCRIPT_DIR=%~dp0
+goto begin
+goto :EOF
+
+:i_python
+  echo python not found, please install python 
+  start "" https://apps.microsoft.com/detail/9pjpw5ldxlz5
+  goto:EOF
+
+:i_git
+  echo git not found, please install git
+  start "" https://gitforwindows.org/
+  goto:EOF
+
+:download
+  echo "downloading" "converter..."
+  git clone --no-checkout https://github.com/it4impuls/mealplan-converter.git tmp/
+  move tmp\.git .git
+  git reset --hard HEAD
+  DEL /S "tmp"
+
+:loop
+    IF NOT exist "%SCRIPT_DIR%*.pdf" (
+        set /P var="keine PDF's gefunden. Bitte fügen Sie die PDF dateien in diesen Ordner ein und drücke dannach enter."
+        )
+        goto loop
 
 :begin
 
-  python --version 2>NUL
-  if not errorlevel 0 goto i_python
+  python --version 2>NUL & IF ERRORLEVEL 1 goto i_python
 
-  git --version 2>NUL
-  if not errorlevel 0 goto i_git
+  git --version 2>NUL & IF ERRORLEVEL 1 goto i_git
 
-  if not exist .git\ goto download
+  if not exist .git\ call :download
 
   if not exist .venv\ (
     echo "installing" "virtual" "environment..."
@@ -24,27 +47,3 @@ SET SCRIPT_DIR=%~dp0
 
   goto loop
   python3 "main.py"
-
-
-
-
-
-:i_python
-  start "" https://apps.microsoft.com/detail/9pjpw5ldxlz5
-
-:i_git
-  start "" https://gitforwindows.org/
-  goto :EOF
-
-:download
-  echo "downloading" "converter..."
-  git clone --no-checkout https://github.com/it4impuls/mealplan-converter.git tmp/
-  move tmp\.git .git
-  git reset --hard HEAD
-  DEL /S "tmp"
-
-:loop
-    IF NOT exist "%SCRIPT_DIR%*.pdf" (
-        set /P var="keine PDF's gefunden. Bitte fügen Sie die PDF dateien in diesen Ordner ein und drücke dannach enter."
-        )
-        goto loop
