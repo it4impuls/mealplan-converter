e0f0fa7e752272ffbb6bf49776435a0510a226d1
marilynias <marilynia57+github@gmail.com>
2024-07-04 14:34:36 +0200

    new win runfile

--- run.bat
+++ run.bat
@@ -1,11 +1,46 @@
-if n[! -d ".venv" ]; then
-  python -m venv .venv
-fi
+@echo off
+SET SCRIPT_DIR=%~dp0
 
-.venv\Scripts\pip install -r requirements.txt
+:i_python
+  WinAppDeployCmd install python
 
-if n[! -f "*.pdf" ]; then
-   read -p "bitte fügen Sie die PDF dateien in diesen Ordner ein und drücke dannach enter."
-fi
+:i_git
+  open "https://gitforwindows.org/"
 
-.venv\Scripts\python main.py
+:download
+  echo "downloading" "converter..."
+  git clone --no-checkout https://github.com/it4impuls/mealplan-converter.git tmp/
+  move tmp\.git .git
+  git reset --hard HEAD
+  DEL /S "tmp"
+
+:loop
+    IF NOT exist "%SCRIPT_DIR%*.pdf" (
+        set /P var="keine PDF's gefunden. Bitte fügen Sie die PDF dateien in diesen Ordner ein und drücke dannach enter."
+        )
+        goto loop
+  
+
+python --version 2>NUL
+if not errorlevel 0 goto i_python
+
+git --version 2>NUL
+if not errorlevel 0 goto i_git
+
+if not exist .git\ goto download
+
+if not exist .venv\ (
+  echo "installing" "virtual" "environment..."
+  python3 -m venv %SCRIPT_DIR%.venv
+  echo "done!n"
+)
+
+source "%SCRIPT_DIR%.venv\bin\activate"
+echo "updating" "dependencies..."
+pip3 "install" "-r" "requirements.txt" 
+echo "done!"
+
+goto loop
+
+REM UNKNOWN: {"type":"While","clause":{"type":"CompoundList","commands":[{"type":"Command","name":{"text":"[","type":"Word"},"suffix":[{"text":"!","type":"Word"},{"text":"ls","type":"Word"},{"text":"SCRIPT_DIR/*.pdf","type":"Word"}],"async":true},{"type":"Command","name":{"text":"]","type":"Word"},"prefix":[{"type":"Redirect","op":{"text":">","type":"great"},"file":{"text":"/dev/null","type":"Word"}}]}]},"do":{"type":"CompoundList","commands":[{"type":"Command","name":{"text":"read","type":"Word"},"suffix":[{"text":"-s","type":"Word"},{"text":"-n","type":"Word"},{"text":"1","type":"Word"},{"text":"-p","type":"Word"},{"text":"keine PDF's gefunden. Bitte fügen Sie die PDF dateien in diesen Ordner ein und drücke dannach enter.","type":"Word"}]}]}}
+python3 "main.py"
