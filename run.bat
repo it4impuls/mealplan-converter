296f7f67b80de0693ef9642df7a1ccb0838acfe3
marilynias <marilynia57+github@gmail.com>
2024-07-04 15:05:07 +0200

    corr

--- run.bat
+++ run.bat
@@ -1,9 +1,10 @@
 @echo off
 SET SCRIPT_DIR=%~dp0
+goto begin
+goto :EOF
 
 :i_python
   start "" https://apps.microsoft.com/detail/9pjpw5ldxlz5
-  goto :EOF
 
 :i_git
   start "" https://gitforwindows.org/
@@ -22,27 +23,26 @@ SET SCRIPT_DIR=%~dp0
         )
         goto loop
 
+:begin
 
-python --version 2>NUL
-if not errorlevel 0 goto i_python
+  python --version 2>NUL
+  if not errorlevel 0 goto i_python
 
-git --version 2>NUL
-if not errorlevel 0 goto i_git
+  git --version 2>NUL
+  if not errorlevel 0 goto i_git
 
-if not exist .git\ goto download
+  if not exist .git\ goto download
 
-if not exist .venv\ (
-  echo "installing" "virtual" "environment..."
-  python3 -m venv %SCRIPT_DIR%.venv
-  echo "done!n"
-)
+  if not exist .venv\ (
+    echo "installing" "virtual" "environment..."
+    python3 -m venv %SCRIPT_DIR%.venv
+    echo "done!n"
+  )
 
-source "%SCRIPT_DIR%.venv\bin\activate"
-echo "updating" "dependencies..."
-pip3 "install" "-r" "requirements.txt" 
-echo "done!"
+  source "%SCRIPT_DIR%.venv\bin\activate"
+  echo "updating" "dependencies..."
+  pip3 "install" "-r" "requirements.txt" 
+  echo "done!"
 
-goto loop
-
-REM UNKNOWN: {"type":"While","clause":{"type":"CompoundList","commands":[{"type":"Command","name":{"text":"[","type":"Word"},"suffix":[{"text":"!","type":"Word"},{"text":"ls","type":"Word"},{"text":"SCRIPT_DIR/*.pdf","type":"Word"}],"async":true},{"type":"Command","name":{"text":"]","type":"Word"},"prefix":[{"type":"Redirect","op":{"text":">","type":"great"},"file":{"text":"/dev/null","type":"Word"}}]}]},"do":{"type":"CompoundList","commands":[{"type":"Command","name":{"text":"read","type":"Word"},"suffix":[{"text":"-s","type":"Word"},{"text":"-n","type":"Word"},{"text":"1","type":"Word"},{"text":"-p","type":"Word"},{"text":"keine PDF's gefunden. Bitte fügen Sie die PDF dateien in diesen Ordner ein und drücke dannach enter.","type":"Word"}]}]}}
-python3 "main.py"
+  goto loop
+  python3 "main.py"
