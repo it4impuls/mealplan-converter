37d55e14a8cacb71ad170f635303810155dfefbe
marilynias <marilynia57+github@gmail.com>
2024-07-05 09:51:13 +0200

    fix bat move

--- run.bat
+++ run.bat
@@ -1,30 +1,39 @@
 @echo off
 SET SCRIPT_DIR=%~dp0
+SET TMP_DIR=tmp\
 goto begin
 goto :EOF
 
 :i_python
   echo python not found, please install python 
   start "" https://apps.microsoft.com/detail/9pjpw5ldxlz5
+  pause
   goto:EOF
 
 :i_git
   echo git not found, please install git
   start "" https://gitforwindows.org/
+  pause
   goto:EOF
 
 :download
-  echo "downloading" "converter..."
-  git clone --no-checkout https://github.com/it4impuls/mealplan-converter.git tmp/
-  move tmp\.git .git
+  echo downloading converter...
+  git clone --no-checkout https://github.com/it4impuls/mealplan-converter.git %TMP_DIR%
+  dir tmp /a
+  xcopy /s /e %SCRIPT_DIR%tmp\.git\* .git\
+  DEL /q /S "tmp"
   git reset --hard HEAD
-  DEL /S "tmp"
+  
 
-:loop
-    IF NOT exist "%SCRIPT_DIR%*.pdf" (
-        set /P var="keine PDF's gefunden. Bitte fügen Sie die PDF dateien in diesen Ordner ein und drücke dannach enter."
-        )
-        goto loop
+:venv
+  echo installing virtual environment...
+  python3 -m venv %SCRIPT_DIR%.venv
+  echo done!n
+
+:find_pdf
+    IF NOT exist "%SCRIPT_DIR%*.pdf" \
+      set /P var="keine PDF's gefunden. Bitte fügen Sie die PDF dateien in diesen Ordner ein und drücke dannach enter."
+      goto find_pdf
 
 :begin
 
@@ -33,17 +42,20 @@ goto :EOF
   git --version 2>NUL & IF ERRORLEVEL 1 goto i_git
 
   if not exist .git\ call :download
-
-  if not exist .venv\ (
-    echo "installing" "virtual" "environment..."
-    python3 -m venv %SCRIPT_DIR%.venv
-    echo "done!n"
-  )
+  goto:EOF
+  if not exist .venv\ call :venv
 
   source "%SCRIPT_DIR%.venv\bin\activate"
-  echo "updating" "dependencies..."
+  echo updating dependencies...
   pip3 "install" "-r" "requirements.txt" 
-  echo "done!"
+  echo done!
 
-  goto loop
+  call :find_pdf
   python3 "main.py"
+
+
+
+
+
+:end
+  
